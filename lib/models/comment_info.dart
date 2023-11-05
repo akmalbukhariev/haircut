bf7b70d7b9e2e3284759930546837732ea9fd3e3
akmalbukhariev <akmalbukhariev@gmail.com>
2023-11-05 22:32:12 +0900

    Created comments page

---
+++ lib/models/comment_info.dart
@@ -0,0 +1,11 @@
+class CommentInfo{
+  final String name;
+  final int starCount;
+  final String date;
+  final String comment;
+
+  CommentInfo({this.name = "",
+    this.starCount = 0,
+    this.date = "",
+    this.comment = ""});
+}
